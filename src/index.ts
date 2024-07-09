033542dab67da95e29c00a23dd6b8ea6150f90a6
huutoanrobotic <huutoanit2023@gmail.com>
2024-07-09 11:59:27 +0700

    update to use Local ipconfig

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import productRoutes from './routes/product';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const port = 4000;
 const app = express();
+const hostname = process.env.HOST_NAME;
 
 app.use(express.json());
 app.use(cors());
@@ -16,8 +20,8 @@ mongoose.connect("mongodb://localhost:27017/db_motocycle");
 app.get("/", (req: Request, res: Response) => {
     res.send("Express app is running")
 })
-app.listen(port, () => {
-    console.log('Server running at Port ' + port);
+app.listen(port, hostname,  () => {
+    console.log(`Máy chủ đang chạy tại http://${hostname}:${port}/`);
 });
 
 app.use('/api/products', productRoutes);
