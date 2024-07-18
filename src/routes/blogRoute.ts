ad230321095327d69549d59fb084f80827799162
huutoanrobotic <huutoanit2023@gmail.com>
2024-07-18 10:54:26 +0700

    update route of routeBlog

--- src/routes/blogRoute.ts
+++ src/routes/blogRoute.ts
@@ -7,7 +7,7 @@ const router: Router = Router();
 router.get('/all-blog', blogController.getAllBlogs);
 router.get('/blog/:id', blogController.getAllBlogs);
 router.post('/add-blog', uploadCloudinary, blogController.addBlog);
-router.put('/update-blog', blogController.updateBlog);
+router.put('/update-blog/:id', blogController.updateBlog);
 router.delete('/blog/:id',  blogController.deleteBlog);
 
 
