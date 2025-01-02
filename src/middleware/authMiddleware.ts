687c6840220a542b63b3beedf9e671a5c8e0f984
huutoan24092000 <138637847+SEHuuToan@users.noreply.github.com>
2025-01-02 23:52:36 +0700

    update token for deploy

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -6,12 +6,13 @@ interface AuthenticatedRequest extends Request {
   }
 const authenticateJWT = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     let tokenHeader = req.headers.authorization;
-    console.log("TokenHeader: ", tokenHeader);
     if (!tokenHeader) {
         return res.status(401).send('Access Denied Wrong TokenHeader');
     }
-    const token = tokenHeader.split(' ')[1];
-    console.log("token: ", token);
+    if(tokenHeader){
+        tokenHeader.split(' ')[1];
+    }
+    const token = tokenHeader
     if (!token) {
         return res.status(401).send('Access Denied Wrong token after split Bearer');
     }
@@ -30,9 +31,11 @@ const authenticateJWT = (req: AuthenticatedRequest, res: Response, next: NextFun
             try {
                 const { id } = verifyToken(refreshToken, 'refresh') as { id: mongoose.ObjectId };
                 const newAccessToken = generateAccessToken(id);
-                res.setHeader('Authorization', `Bearer ${newAccessToken}`);
+                res.setHeader('Authorization',  `Bearer ${newAccessToken}`);
                 // Add new access token to request headers
-                req.headers.authorization = `Bearer ${newAccessToken}`;
+                req.headers.authorization =  `Bearer ${newAccessToken}`;
+                // Retry the original request with the new token
+                req.headers.authorization =  `Bearer ${newAccessToken}`;
                 return next();
             } catch (error) {
                 console.error('Refresh token verification failed:', error);
